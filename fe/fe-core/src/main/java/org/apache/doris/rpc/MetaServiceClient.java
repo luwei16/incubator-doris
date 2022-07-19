85fab727ddae006da57390498816c5f68e09eb44
deardeng <565620795@qq.com>
2022-07-19 10:09:20 +0800

    [feature](selectdb-cloud) fe client to meta service (#441)

---
+++ fe/fe-core/src/main/java/org/apache/doris/rpc/MetaServiceClient.java
@@ -0,0 +1,64 @@
+package org.apache.doris.rpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+import org.apache.doris.common.Config;
+import org.apache.doris.proto.MetaService;
+import org.apache.doris.proto.MetaService.PGetVisibleVersionResponse;
+import org.apache.doris.proto.PMetaServiceGrpc;
+import org.apache.doris.thrift.TNetworkAddress;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class MetaServiceClient {
+    public static final Logger LOG = LogManager.getLogger(BackendServiceClient.class);
+
+    private static final int MAX_RETRY_NUM = 0;
+    private final TNetworkAddress address;
+    private final PMetaServiceGrpc.PMetaServiceFutureStub stub;
+    private final PMetaServiceGrpc.PMetaServiceBlockingStub blockingStub;
+    private final ManagedChannel channel;
+
+    public MetaServiceClient(TNetworkAddress address) {
+        this.address = address;
+        channel = NettyChannelBuilder.forAddress(address.getHostname(), address.getPort())
+            .flowControlWindow(Config.grpc_max_message_size_bytes)
+            .maxInboundMessageSize(Config.grpc_max_message_size_bytes).enableRetry().maxRetryAttempts(MAX_RETRY_NUM)
+            .usePlaintext().build();
+        stub = PMetaServiceGrpc.newFutureStub(channel);
+        blockingStub = PMetaServiceGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() {
+        if (!channel.isShutdown()) {
+            channel.shutdown();
+            try {
+                if (!channel.awaitTermination(5, TimeUnit.SECONDS)) {
+                    LOG.warn("Timed out gracefully shutting down connection: {}. ", channel);
+                }
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+
+        if (!channel.isTerminated()) {
+            channel.shutdownNow();
+            try {
+                if (!channel.awaitTermination(5, TimeUnit.SECONDS)) {
+                    LOG.warn("Timed out forcefully shutting down connection: {}. ", channel);
+                }
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+
+        LOG.warn("shut down backend service client: {}", address);
+    }
+
+    public Future<PGetVisibleVersionResponse> getVisibleVersionAsync(MetaService.PGetVisibleVersionRequest request) {
+        return stub.getVisibleVersion(request);
+    }
+}
---
+++ fe/fe-core/src/main/java/org/apache/doris/rpc/MetaServiceProxy.java
@@ -0,0 +1,80 @@
+package org.apache.doris.rpc;
+
+import com.google.common.collect.Maps;
+import org.apache.doris.proto.MetaService.PGetVisibleVersionRequest;
+import org.apache.doris.proto.MetaService.PGetVisibleVersionResponse;
+import org.apache.doris.thrift.TNetworkAddress;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.thrift.TException;
+
+import java.util.Map;
+import java.util.concurrent.Future;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MetaServiceProxy {
+    private static final Logger LOG = LogManager.getLogger(MetaServiceProxy.class);
+    // use exclusive lock to make sure only one thread can add or remove client from serviceMap.
+    // use concurrent map to allow access serviceMap in multi thread.
+    private ReentrantLock lock = new ReentrantLock();
+
+    private final Map<TNetworkAddress, MetaServiceClient> serviceMap;
+
+    public MetaServiceProxy() {
+        this.serviceMap = Maps.newConcurrentMap();
+    }
+
+    private static class SingletonHolder {
+        private static final MetaServiceProxy INSTANCE = new MetaServiceProxy();
+    }
+
+    public static MetaServiceProxy getInstance() {
+        return MetaServiceProxy.SingletonHolder.INSTANCE;
+    }
+
+    public void removeProxy(TNetworkAddress address) {
+        LOG.warn("begin to remove proxy: {}", address);
+        MetaServiceClient service;
+        lock.lock();
+        try {
+            service = serviceMap.remove(address);
+        } finally {
+            lock.unlock();
+        }
+
+        if (service != null) {
+            service.shutdown();
+        }
+    }
+
+    private MetaServiceClient getProxy(TNetworkAddress address) {
+        MetaServiceClient service = serviceMap.get(address);
+        if (service != null) {
+            return service;
+        }
+
+        // not exist, create one and return.
+        lock.lock();
+        try {
+            service = serviceMap.get(address);
+            if (service == null) {
+                service = new MetaServiceClient(address);
+                serviceMap.put(address, service);
+            }
+            return service;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Future<PGetVisibleVersionResponse> getVisibleVersionAsync(TNetworkAddress address,
+                                                                     PGetVisibleVersionRequest request)
+        throws TException, RpcException {
+        try {
+            final MetaServiceClient client = getProxy(address);
+            return client.getVisibleVersionAsync(request);
+        } catch (Exception e) {
+            throw new RpcException(address.hostname, e.getMessage(), e);
+        }
+    }
+}
