57ccefdd2d915652ce99d379a6106573133e7fcc
Lightman <31928846+Lchangliang@users.noreply.github.com>
2022-10-28 20:47:59 +0800

    [feature](selectdb-cloud) Modify selectdb version display on MySQL client (#949)

--- fe/fe-core/src/main/java/org/apache/doris/qe/GlobalVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/GlobalVariable.java
@@ -50,8 +50,7 @@ public final class GlobalVariable {
     public static final long VALIDATE_PASSWORD_POLICY_STRONG = 2;
 
     @VariableMgr.VarAttr(name = VERSION_COMMENT, flag = VariableMgr.READ_ONLY)
-    public static String versionComment = "SelectDB version: "
-            + Version.DORIS_BUILD_VERSION + "-" + Version.DORIS_BUILD_SHORT_HASH;
+    public static String versionComment = "SelectDB Core version: " + Version.DORIS_BUILD_VERSION;
 
     @VariableMgr.VarAttr(name = VERSION, flag = VariableMgr.READ_ONLY)
     public static String version = MysqlHandshakePacket.SERVER_VERSION;
