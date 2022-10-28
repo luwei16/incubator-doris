25372269c7f9716f3e45e1b0aa767f21d77cb7af
meiyi <myimeiyi@gmail.com>
2022-10-28 22:43:55 +0800

    [feature](selectdb-cloud) Fix the unstable load of data that ends with a column separator (#954)
    Fix the unstable load of data that ends with a column separator by setting BrokerScanNode rangeDesc ReadByColumnDef to true.

--- fe/fe-core/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-core/src/main/java/org/apache/doris/common/Config.java
@@ -1860,4 +1860,7 @@ public class Config extends ConfigBase {
 
     @ConfField(mutable = false)
     public static int cloud_copy_txn_conflict_error_retry_num = 5;
+
+    @ConfField
+    public static boolean range_desc_read_by_column_def = true;
 }
