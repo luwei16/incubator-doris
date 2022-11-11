42f45c771197b8c58a5ab7148c127af933ade865
Lei Zhang <27994433+SWJTU-ZhangLei@users.noreply.github.com>
2022-11-11 23:57:21 +0800

    [feature](selectdb-cloud) Enhance start.sh/stop.sh for selectdb_cloud (#1079)
    (cherry picked from commit afb19c0a8e0681ca02ce266fb726d120dd9ee59d)

--- cloud/script/start.sh
+++ cloud/script/start.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
+curdir="$(cd "$(dirname $(readlink -f ${BASH_SOURCE[0]}))" &>/dev/null && pwd)"
+selectdb_home="$(
+    cd "${curdir}/.."
+    pwd
+)"
+
+cd ${selectdb_home}
 
 if [[ ! -d bin || ! -d conf || ! -d lib ]]; then
 	echo "$0 must be invoked at the directory which contains bin, conf and lib"
@@ -10,35 +17,34 @@ for arg do
   shift
   [ "$arg" = "--daemonized" ] && daemonized=1 && continue
   [ "$arg" = "-daemonized" ] && daemonized=1 && continue
+  [ "$arg" = "--daemon" ] && daemonized=1 && continue
   set -- "$@" "$arg"
 done
 # echo "$@" "daemonized=${daemonized}"}
 
 process=selectdb_cloud
 
-if [ -f bin/${process}.pid ]; then
+if [ -f ${selectdb_home}/bin/${process}.pid ]; then
 	echo "pid file existed, ${process} may have already started"
 	exit -1;
 fi
 
-if ! command -v patchelf &> /dev/null; then
-	echo "patchelf is needed to launch meta_service"
-	exit -1
-fi
-
-lib_path=$(pwd)/lib
-bin=$(pwd)/lib/selectdb_cloud
+lib_path=${selectdb_home}/lib
+bin=${selectdb_home}/lib/selectdb_cloud
 ldd ${bin} | grep -Ei 'libfdb_c.*not found' &> /dev/null
 if [ $? -eq 0 ]; then
+    if ! command -v patchelf &> /dev/null; then
+        echo "patchelf is needed to launch meta_service"
+        exit -1
+    fi
 	patchelf --set-rpath ${lib_path} ${bin}
 	ldd ${bin}
 fi
 
-mkdir -p log
-
+mkdir -p ${selectdb_home}/log
 echo "starts ${process} with args: $@"
 if [ ${daemonized} -eq 1 ]; then
-    nohup ${bin} "$@" > log/${process}.out 2>&1 &
+    nohup ${bin} "$@" > ${selectdb_home}/log/${process}.out 2>&1 &
 else
     ${bin} "$@"
 fi
--- cloud/script/stop.sh
+++ cloud/script/stop.sh
@@ -1,13 +1,19 @@
-#!/bin/bash
-cd `dirname $0`
+#!/usr/bin/bash
+curdir="$(cd "$(dirname $(readlink -f ${BASH_SOURCE[0]}))" &>/dev/null && pwd)"
+selectdb_home="$(
+    cd "${curdir}/.."
+    pwd
+)"
+
+cd ${selectdb_home}
 
 process=selectdb_cloud
 
-if [ ! -f ${process}.pid ]; then
+if [ ! -f ${selectdb_home}/bin/${process}.pid ]; then
 	echo "no ${process}.pid found, process may have been stopped"
 	exit -1
 fi
 
-pid=`cat ${process}.pid`
+pid=`cat ${selectdb_home}/bin/${process}.pid`
 kill -2 ${pid}
-rm -f ${process}.pid
+rm -f ${selectdb_home}/bin/${process}.pid
