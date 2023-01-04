b87e87c10db9a2cb3f003782c7ec5327b3b2d4b9
Lightman <31928846+Lchangliang@users.noreply.github.com>
2023-01-04 14:58:17 +0800

    [feature](selectdb-cloud) Fix BE cache metrics regression test (#1306)

--- regression-test/suites/cloud/cache/cache_metrics.groovy
+++ regression-test/suites/cloud/cache/cache_metrics.groovy
@@ -3,36 +3,38 @@ import org.codehaus.groovy.runtime.IOGroovyMethods
 suite("test_cache_metrics") {
     String[][] backends = sql """ show backends; """
     assertTrue(backends.size() > 0)
-    String backend_id;
-    def backendId_to_backendIP = [:]
-    def backendId_to_backendHttpPort = [:]
+    String backendId;
+    def backendIdToBackendIP = [:]
+    def backendIdToBackendHttpPort = [:]
     for (String[] backend in backends) {
         if (backend[9].equals("true")) {
-            backendId_to_backendIP.put(backend[0], backend[2])
-            backendId_to_backendHttpPort.put(backend[0], backend[5])
+            backendIdToBackendIP.put(backend[0], backend[2])
+            backendIdToBackendHttpPort.put(backend[0], backend[5])
         }
     }
 
-    backend_id = backendId_to_backendIP.keySet()[0]
-    StringBuilder showConfigCommand = new StringBuilder();
-    showConfigCommand.append("curl -X GET http://")
-    showConfigCommand.append(backendId_to_backendIP.get(backend_id))
-    showConfigCommand.append(":")
-    showConfigCommand.append(backendId_to_backendHttpPort.get(backend_id))
-    showConfigCommand.append("/metrics")
-    logger.info(showConfigCommand.toString())
-    def process = showConfigCommand.toString().execute()
-    int code = process.waitFor()
-    String err = IOGroovyMethods.getText(new BufferedReader(new InputStreamReader(process.getErrorStream())));
-    String out = process.getText()
-    assertEquals(code, 0)
-    def strs = out.split('\n')
-    Boolean flag = false;
-    for (String line in strs) {
-        if (line.startsWith("doris_be_cache")) {
-            flag = true;
-            break;
+    backendId = backendIdToBackendIP.keySet()[0]
+    def getMetricsMethod = { check_func ->
+        httpTest {
+            endpoint backendIdToBackendIP.get(backendId) + ":" + backendIdToBackendHttpPort.get(backendId)
+            uri "/metrics"
+            op "get"
+            check check_func
         }
     }
-    assertTrue(flag);
+
+    getMetricsMethod.call() {
+        respCode, body ->
+            assertEquals("${respCode}".toString(), "200")
+            String out = "${body}".toString()
+            def strs = out.split('\n')
+            Boolean flag = false;
+            for (String line in strs) {
+                if (line.startsWith("doris_be_cache")) {
+                    flag = true;
+                    break;
+                }
+            }
+            assertTrue(flag);
+    }
 }
