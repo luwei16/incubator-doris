663ef673ffcfe3742a18c4b9a303f39055758655
Tiewei Fang <43782773+BePPPower@users.noreply.github.com>
2023-01-06 11:00:59 +0800

    [feature](multi-catalog) support postgresql jdbc catalog (#15570)
    support postgresql jdbc catalog

--- fe/fe-core/src/main/java/org/apache/doris/qe/ShowExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/ShowExecutor.java
@@ -2391,6 +2391,9 @@ public class ShowExecutor {
 
     public void handleShowCatalogs() throws AnalysisException {
         ShowCatalogStmt showStmt = (ShowCatalogStmt) stmt;
+        if (ctx.getCurrentCatalog() == null) {
+            throw new AnalysisException("Current catalog is not exist, please switch catalog.");
+        }
         resultSet = Env.getCurrentEnv().getCatalogMgr().showCatalogs(showStmt, ctx.getCurrentCatalog().getName());
     }
 
