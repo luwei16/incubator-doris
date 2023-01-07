c19c5a62fc6c829eb19dc9f050ade0b510e0c2e5
Kang <kxiao.tiger@gmail.com>
2023-01-08 00:16:01 +0800

    revert jsonb simdjson-3.0.1 to 1.0.2 (#1318)
    * revert simdjson-3.0.1 to 1.0.2

--- be/src/vec/functions/function_jsonb.cpp
+++ be/src/vec/functions/function_jsonb.cpp
@@ -48,7 +48,7 @@ enum class JsonbParseErrorMode { FAIL = 0, RETURN_NULL, RETURN_VALUE, RETURN_INV
 template <NullalbeMode nullable_mode, JsonbParseErrorMode parse_error_handle_mode>
 class FunctionJsonbParseBase : public IFunction {
 private:
-    JsonbParserSIMD default_value_parser;
+    JsonbParser default_value_parser;
     bool has_const_default_value = false;
 
 public:
@@ -195,7 +195,7 @@ public:
         col_to->reserve(size);
 
         // parser can be reused for performance
-        JsonbParserSIMD parser;
+        JsonbParser parser;
         JsonbErrType error = JsonbErrType::E_NONE;
 
         for (size_t i = 0; i < input_rows_count; ++i) {
