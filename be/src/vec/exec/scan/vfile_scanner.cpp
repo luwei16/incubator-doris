cbf0407574c15ec3f71663652deda78111f67625
AlexYue <yj976240184@qq.com>
2023-02-16 14:55:25 +0800

    [enhance](profile) Add profile for json and csv file reader (#1408)
    Previously there are no profile to monitor the time consumption of data converting to doris column, this pr mainly adds profile for json file and csv file to do so.
    Plus, there was no good way for us to detect which json callback used in JsonReader, i add one log to print the information.
    And formerly the FileReadTime profile is repetitive, which would result in one larger time than actually used.

--- be/src/vec/exec/scan/vfile_scanner.cpp
+++ be/src/vec/exec/scan/vfile_scanner.cpp
@@ -72,6 +72,7 @@ Status VFileScanner::prepare(
     _pre_filter_timer = ADD_TIMER(_parent->_scanner_profile, "FileScannerPreFilterTimer");
     _convert_to_output_block_timer =
             ADD_TIMER(_parent->_scanner_profile, "FileScannerConvertOuputBlockTime");
+    _next_reader_timer = ADD_TIMER(_parent->_scanner_profile, "GetNextReaderTime");
 
     if (vconjunct_ctx_ptr != nullptr) {
         // Copy vconjunct_ctx_ptr from scan node to this scanner's _vconjunct_ctx.
@@ -153,6 +154,7 @@ Status VFileScanner::_handle_dynamic_block(Block* block) {
 Status VFileScanner::_get_block_impl(RuntimeState* state, Block* block, bool* eof) {
     do {
         if (_cur_reader == nullptr || _cur_reader_eof) {
+            SCOPED_TIMER(_next_reader_timer);
             RETURN_IF_ERROR(_get_next_reader());
         }
 
