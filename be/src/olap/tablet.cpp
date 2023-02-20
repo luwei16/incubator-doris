626e3de4823beb93767c293af316201d58aa7a19
plat1ko <platonekosama@gmail.com>
2023-02-20 21:47:32 +0800

    [debug](version-graph) Log when capture_consistent_versions failed (#1424)
    [debug](version-graph) Log when capture_consistent_versions failed

--- be/src/olap/tablet.cpp
+++ be/src/olap/tablet.cpp
@@ -562,7 +562,8 @@ Status Tablet::cloud_capture_rs_readers(const Version& version_range,
     auto st = _timestamped_version_tracker.capture_consistent_versions(version_range, &version_path);
     if (!st.ok()) {
         st.append(" tablet_id=" + std::to_string(tablet_id()));
-        VLOG_DEBUG << st << '\n' << [this]() { std::string json; get_compaction_status(&json); return json; }();
+        LOG(WARNING) << st << '\n' << [this]() { std::string json; get_compaction_status(&json); return json; }();
+        DCHECK(st.ok()) << st;
         return st;
     }
     // clang-format on
