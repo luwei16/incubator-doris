e6aac186c155adcb433e4ca3849b67b724de8e3d
deardeng <565620795@qq.com>
2023-02-22 23:02:12 +0800

    [feature](selectdb-cloud) Add smoke test case for oss internal endpoint (#1427)

--- regression-test/suites/cloud/smoke/copy_into/test_http_data_api.groovy
+++ regression-test/suites/cloud/smoke/copy_into/test_http_data_api.groovy
@@ -17,8 +17,50 @@ suite("test_internal_stage_http_data_api", "smoke") {
     def code = process.waitFor()
     def err = IOGroovyMethods.getText(new BufferedReader(new InputStreamReader(process.getErrorStream())));
     def out = process.getText()
-    logger.info("Request FE Config: code=" + code + ", out=" + out + ", err=" + err)
+    logger.info("Http data api Request FE Config: code=" + code + ", out=" + out + ", err=" + err)
     assertEquals(code, 0)
+
+    // if aliyun, test internal endpoint
+    if ("oss".equals(getProvider())) {
+        def tmp = new StringBuilder(strBuilder)
+        // test internal
+        strBuilder.append(""" -H Host:www.selectdb.com --connect-timeout 5 -vv""")
+        command = strBuilder.toString()
+        logger.info("Test aliyun internal endpoint command=" + command)
+        process = command.toString().execute()
+        code = process.waitFor()
+        err = IOGroovyMethods.getText(new BufferedReader(new InputStreamReader(process.getErrorStream())));
+        out = process.getText()
+        def location = null
+        err.eachLine {if(it.startsWith("< Location")) {
+            location = it
+        }}
+        if (null == location) {
+            assertTrue(false)
+        }
+        logger.info("location: " + location)
+        // < Location: http://selectdb-online-cn-shenzhen-bucket1667494333.oss-cn-shenzhen-internal.aliyuncs.com/m-xoad88n48839bauewv/stage/admin/admin/internal_customer.csv?Expires=1676956170&OSSAccessKeyId=LTAI5tCRA18qGLf6CiD9DFRv&Signature=GVFAAhSyNnZzFpXaakoc8gNietg%3D
+        // oss-cn-shenzhen-internal, assert contain '-internal'
+        assertTrue(location.toString().contains("-internal"))
+
+        // test non internal, host is ip, so non internal
+        tmp.append(""" -H Host:210.211.212.213:8888 --connect-timeout 5 -vv""")
+        command = tmp.toString()
+        logger.info("Test aliyun non internal endpoint command=" + command)
+        process = command.toString().execute()
+        code = process.waitFor()
+        err = IOGroovyMethods.getText(new BufferedReader(new InputStreamReader(process.getErrorStream())));
+        out = process.getText()
+        location = null
+        err.eachLine {if(it.startsWith("< Location")) {
+            location = it
+        }}
+        if (null == location) {
+            assertTrue(false)
+        }
+        // curl -u admin:smoke_Test -H fileName:internal_customer.csv -T /mnt/disk3/smoke-test/bin/regression-test/data/cloud/smoke/copy_into/internal_customer.csv -L http://119.23.41.172:39590/copy/upload -H Host:210.211.212.213:8888 --connect-timeout 5 -vv
+        assertFalse(location.toString().contains("-internal"))
+    }
 
     try {
         sql """ DROP TABLE IF EXISTS ${tableName}; """
