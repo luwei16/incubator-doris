69b7f5fc130229a86e474f802359685bf1a0a663
Luwei <814383175@qq.com>
2023-04-06 22:57:04 +0800

    Fix some regression cases to adapt to cloud mode (#1616)

--- regression-test/suites/partition_p0/dynamic_partition/test_dynamic_partition.groovy
+++ regression-test/suites/partition_p0/dynamic_partition/test_dynamic_partition.groovy
@@ -55,8 +55,10 @@ suite("test_dynamic_partition") {
             "dynamic_partition.create_history_partition"="true",
             "dynamic_partition.replication_allocation" = "tag.location.not_exist_tag: 1")
         """
+        if (context.config.metaServiceHttpAddress.isEmpty()) {
         // check exception message contains
         exception "errCode = 2,"
+        }
     }
     sql "drop table if exists dy_par_bad"
 
@@ -97,8 +99,10 @@ suite("test_dynamic_partition") {
             "dynamic_partition.create_history_partition"="true",
             "dynamic_partition.replication_allocation" = "tag.location.not_exist_tag: 1")
         """
+        if (context.config.metaServiceHttpAddress.isEmpty()) {
         // check exception message contains
         exception "errCode = 2,"
+        }
     }
     sql "drop table if exists dy_par_bad"
 }
